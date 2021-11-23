9b56c70c4eeae3d78b79979f86f13136fb30ba47
Clément <csapt2001@gmail.com>
2021-11-23 15:31:08 +0100

    add update method and profile page :heavy_plus_sign:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.friendly.find(params[:id])
+  end
+
+  def profile
+    @user = current_user
+  end
+
+  def update
+    #stimulus js ?
+    @user = User.friendly.find(params[:id])
+    @user.update(params_user)
+    redirect_to profile_path
+  end
+
   def spotify
     @spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    @recommendations = RSpotify::Recommendations.generate(seed_genres: ['hip-hop', 'french'])
+  end
+
+  private
+  def params_user
+    params.require(:user).permit(:job, :photo)
   end
 end
