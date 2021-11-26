2d63e3bf024ca31544bb8510911f1d6c48db7b25
Clément <csapt2001@gmail.com>
2021-11-26 13:57:53 +0100

    delete wrong migration :fire:

--- db/migrate/20211126124756_delete_weight_from_answers.rb
+++
@@ -1,5 +0,0 @@
-class DeleteWeightFromAnswers < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :answers, :weight
-  end
-end
--- db/migrate/20211126124805_delete_weight_from_questions.rb
+++
@@ -1,5 +0,0 @@
-class DeleteWeightFromQuestions < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :questions, :weight
-  end
-end
