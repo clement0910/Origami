f5ea1e2d462eae073e30d67025746871bcb4e53c
Nicolas Mercier <mercier.ncls@gmail.com>
2021-11-26 13:27:25 +0100

    seeds + answers_weight

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_144117) do
+ActiveRecord::Schema.define(version: 2021_11_25_160053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2021_11_24_144117) do
     t.bigint "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "weight", precision: 3, scale: 3
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
