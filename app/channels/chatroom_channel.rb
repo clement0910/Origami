143e7b2db5878da8e042bef8abfc12ca58136e36
gael tardy <gael.tardy3@gmail.com>
2021-11-29 12:03:25 +0100

    actionCable

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -1,9 +1,9 @@
 class ChatroomChannel < ApplicationCable::Channel
   def subscribed
-    def subscribed
-      chatroom = Chatroom.find(params[:id])
-      stream_for chatroom
-    end
+    ap  "je suis la"
+    ap params
+    chatroom = Chatroom.find(params[:id])
+    stream_for chatroom
   end
 
   def unsubscribed
