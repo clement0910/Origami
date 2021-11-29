c309c18837cb9765e4add90771bee6521541f7b7
tom rosiere <tom.rosiere@hotmail.fr>
2021-11-29 14:05:49 +0100

    f

--- app/javascript/controllers/toggle_field_registration_controller.js
+++ app/javascript/controllers/toggle_field_registration_controller.js
@@ -4,22 +4,30 @@ export default class extends Controller {
   static targets = ['email', 'emailbut', 'emailin', 'password', 'passwordin', 'passwordbut', 'passwordconfin', 'name', 'namein', 'namebut', 'gender', 'genderbut', 'genderin', 'birthday', 'birthdayin', 'birthdaybut', 'city', 'cityin', 'citybut', 'photo', 'submit'];
 
     showEmail(event) {
-      if (event.keyCode === 13 || event.target.id === 'next') {
+      if ((event.keyCode === 13 || event.target.id === 'next') && this.emailinTarget.value.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/) != null) {
         this.emailTarget.classList.toggle('none');
         this.passwordTarget.classList.toggle('none')
       }
-      if (event.keyCode > 0 && this.emailinTarget.value.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/) != null) {
-        this.emailbutTarget.classList = 'btn btn-color';
+      else if ((event.keyCode === 13 || event.target.id === 'next') && this.emailinTarget.classList.contains("red") === false) {
+        this.emailinTarget.classList.toggle('red')
+      }
+      if (event.keyCode > 0 && this.emailinTarget.classList.contains("red")) {
+        this.emailinTarget.classList.toggle('red')
       }
     }
 
     showPassword(event) {
-      if ((event.keyCode === 13 || event.target.id === 'next') && this.passwordinTarget.value.length >= 6) {
+      if ((event.keyCode === 13 || event.target.id === 'next') && this.passwordinTarget.value.length >= 6 && this.passwordconfinTarget.value.length >= 6 && this.passwordinTarget.value === this.passwordconfinTarget.value) {
         this.passwordTarget.classList.toggle('none');
         this.nameTarget.classList.toggle('none')
       }
-      if (event.keyCode > 0 && this.passwordinTarget.value.length >= 6 && this.passwordconfinTarget.value.length >= 6) {
-        this.passwordbutTarget.classList = 'btn btn-color';
+      else if ((event.keyCode === 13 || event.target.id === 'next') && this.passwordinTarget.classList.contains("red") === false) {
+        this.passwordinTarget.classList.toggle("red");
+        this.passwordconfinTarget.classList.toggle("red")
+      }
+      if (event.keyCode > 0 && this.passwordinTarget.classList.contains("red")) {
+        this.passwordinTarget.classList.toggle("red");
+        this.passwordconfinTarget.classList.toggle("red")
       }
     }
 
@@ -28,8 +36,11 @@ export default class extends Controller {
         this.nameTarget.classList.toggle('none');
         this.genderTarget.classList.toggle('none')
       }
-      if (event.keyCode > 0 && this.nameinTarget.value.length >= 2) {
-        this.namebutTarget.classList = 'btn btn-color';
+      else if (event.keyCode === 13 || event.target.id === 'next') {
+        this.nameinTarget.classList.toggle("red");
+      }
+      if (event.keyCode > 0 && this.nameinTarget.classList.contains("red")) {
+        this.nameinTarget.classList.toggle("red");
       }
     }
 
