75977bd630cbf1cacd9909164eab11346e865edc
Clément <csapt2001@gmail.com>
2021-12-02 18:10:17 +0100

    fix js :art:

--- app/javascript/plugins/preview-photo.js
+++ app/javascript/plugins/preview-photo.js
@@ -1,12 +1,18 @@
-const input = document.querySelector('.input-photo')
-const img = document.querySelector('#img-preview')
-const plus = document.querySelector('#img-plus')
-input.addEventListener('change', evt => {
-    const file = input.files[0]
-    console.log("cici")
-    console.log(file)
-    if (file) {
-        img.src = URL.createObjectURL(file)
-        plus.classList.add('none')
+
+const previewPhoto = () => {
+
+    const input = document.querySelector('.input-photo')
+    const img = document.querySelector('#img-preview')
+    const plus = document.querySelector('#img-plus')
+
+    if (input) {
+        input.addEventListener('change', evt => {
+            const file = input.files[0]
+            if (file) {
+                img.src = URL.createObjectURL(file)
+                plus.classList.add('none')
+            }
+        })
     }
-})
+}
+export { previewPhoto };
